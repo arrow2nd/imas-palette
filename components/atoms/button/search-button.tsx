336ee422ce2e0df3871500bbc7753cf9d807932e
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-07-15 21:01:02 +0900

    change: color

--- components/atoms/button/search-button.tsx
+++ components/atoms/button/search-button.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 const SearchButton = ({ className = '' }: Props) => (
   <div className={className}>
-    <button className="w-20 h-10 block rounded-md shadow-md bg-black text-white">
+    <button className="w-20 h-10 block rounded-md shadow-md bg-natural-black text-white">
       検索
     </button>
   </div>
