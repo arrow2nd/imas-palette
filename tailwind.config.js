fc6b60fbe0ecd20e26933fb8908029bae0527243
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-07-16 08:49:17 +0900

    add: extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ module.exports = {
     screens: {
       sm: '640px',
       md: '768px'
+    },
+    extend: {
+      colors: {
+        'natural-white': '#faf8f7',
+        'natural-black': '#1c1c1c'
+      }
     }
   },
   variants: {
