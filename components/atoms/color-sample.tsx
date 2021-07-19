dba4359be28259d7dddf37fc7dd13e346c934f8e
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-07-19 11:38:29 +0900

    add: border

--- components/atoms/color-sample.tsx
+++ components/atoms/color-sample.tsx
@@ -7,7 +7,12 @@ const ColorSample = ({ color }: Props) => {
     backgroundColor: color
   }
 
-  return <div className="w-16 h-16 rounded-xl" style={style} />
+  return (
+    <div
+      className="w-16 h-16 rounded-xl border border-gray-200"
+      style={style}
+    />
+  )
 }
 
 export default ColorSample
