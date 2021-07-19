68d5c5bfb8da71f66ab15077e64cee39047bbdeb
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-07-20 00:17:37 +0900

    add: Clear input when changing search conditions

--- components/organisms/search.tsx
+++ components/organisms/search.tsx
@@ -18,27 +18,37 @@ const Search = ({
   onChangeBland,
   onChangeName
 }: Props) => {
-  const nameRef = useRef<HTMLInputElement>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const divClassName = isMobile ? 'flex-col w-full items-center' : 'flex-row'
   const selectClassName = isMobile ? 'w-full' : 'w-64'
   const inputClassName = isMobile ? 'mt-3 w-full' : 'ml-5 w-64'
   const buttonClassName = isMobile ? 'mt-3 w-full' : 'ml-5 w-24'
 
+  const handleChangeBland = (bland: string) => {
+    // 名前の入力を削除
+    if (inputRef.current) {
+      inputRef.current.value = ''
+      onChangeName('')
+    }
+
+    onChangeBland(bland)
+  }
+
   const handleSubmitName = () => {
-    const name = nameRef.current?.value || ''
+    const name = inputRef.current?.value || ''
     onChangeName(name)
   }
 
   return (
     <div className={`flex flex-wrap ${divClassName} ${className}`}>
-      <Select className={selectClassName} onChange={onChangeBland}>
+      <Select className={selectClassName} onChange={handleChangeBland}>
         {options}
       </Select>
       <Input
         className={inputClassName}
         placeholder="アイドル名（ひらがな可）"
-        ref={nameRef}
+        ref={inputRef}
         onSubmit={handleSubmitName}
       />
       <Button className={buttonClassName} onClick={handleSubmitName}>
