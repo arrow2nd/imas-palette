9580a066cdea0ebf4a7382b0175a98e9a5297a3c
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-07-20 13:07:04 +0900

    change: desc

--- data/seo-data.ts
+++ data/seo-data.ts
@@ -2,7 +2,7 @@ import { SeoInfo } from '../types/seo'
 
 export const seoData: SeoInfo = {
   title: 'im@s-palette | アイマスカラーコード検索',
-  desc: 'THE IDOLM@STERシリーズに登場するアイドルの個人カラーを検索できるサイト',
+  desc: 'THE IDOLM@STERシリーズに登場するアイドルの個人カラーが検索できるWebアプリ',
   url: 'https://imas-palette.vercel.app',
   ogpImgUrl: 'https://imas-palette.vercel.app/ogp.png'
 }
