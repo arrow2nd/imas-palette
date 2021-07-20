f117097c2d86ee77b127cc1ae245607e46bcd123
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-07-20 09:57:35 +0900

    add: ogp image

--- data/seo-data.ts
+++ data/seo-data.ts
@@ -1,8 +1,8 @@
 import { SeoInfo } from '../types/seo'
 
 export const seoData: SeoInfo = {
-  title: 'im@s-palette | アイマス色検索',
+  title: 'im@s-palette | アイマスカラーコード検索',
   desc: 'THE IDOLM@STERシリーズに登場するアイドルの個人カラーを検索できるサイト',
   url: 'https://imas-palette.vercel.app',
-  ogpImgUrl: ''
+  ogpImgUrl: 'https://imas-palette.vercel.app/ogp.png'
 }
