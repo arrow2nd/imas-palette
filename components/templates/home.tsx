260dca8604c80f5d674464e5659ff847a8525233
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-09-18 21:41:47 +0900

    clean

--- components/templates/home.tsx
+++ components/templates/home.tsx
@@ -7,18 +7,15 @@ import UI from '../organisms/ui'
 
 type Props = {
   seo: SeoInfoType
-  optionList: OptionType[]
 }
 
-const Home = ({ seo, optionList }: Props) => {
-  return (
-    <div className="flex flex-col mx-8 my-12 min-h-screen">
-      <SEO {...seo} />
-      <Header desc={seo.desc} />
-      <UI optionList={optionList} />
-      <Footer />
-    </div>
-  )
-}
+const Home = ({ seo }: Props) => (
+  <div className="flex flex-col mx-8 my-12 min-h-screen">
+    <SEO {...seo} />
+    <Header desc={seo.desc} />
+    <UI />
+    <Footer />
+  </div>
+)
 
 export default Home
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { seoData } from '../data/seo'
-import { blandList } from '../data/bland-list'
 import Home from '../components/templates/home'
 
-const ImasPalette = () => <Home seo={seoData} optionList={blandList} />
+const ImasPalette = () => <Home seo={seoData} />
 
 export default ImasPalette
