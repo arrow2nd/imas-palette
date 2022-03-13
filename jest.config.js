872ab6513be19396cbd5c60ef9f7a73008551056
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-03-13 22:07:37 +0900

    chore: jestを追加

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './'
+})
+
+const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/', '/.yarn/'],
+  testEnvironment: 'jest-environment-jsdom'
+}
+
+module.exports = createJestConfig(customJestConfig)
