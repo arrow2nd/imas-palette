da1415e296b2294031d21508ca01c372866e35a2
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-03-17 21:25:07 +0900

    test: ユニット名検索のテストを追加

--- hooks/useIdolData.test.ts
+++ hooks/useIdolData.test.ts
@@ -12,11 +12,12 @@ describe('useIdolData', () => {
   })
 
   test.each`
-    testName        | brand            | name        | similarColor | keeps                                                       | expected
-    ${'ブランド名'} | ${'DearlyStars'} | ${''}       | ${''}        | ${[]}                                                       | ${['秋月涼', '日高愛', '水谷絵理']}
-    ${'アイドル名'} | ${''}            | ${'菊地真'} | ${''}        | ${[]}                                                       | ${['菊地真']}
-    ${'色味'}       | ${'DearlyStars'} | ${''}       | ${'#ff0000'} | ${[]}                                                       | ${['日高愛']}
-    ${'keep済み'}   | ${'keep'}        | ${''}       | ${'#800080'} | ${['mei_izumi_shinycolors', 'hinana_ichikawa_shinycolors']} | ${['和泉愛依']}
+    testName        | brand            | name            | similarColor | keeps                                                       | expected
+    ${'ブランド名'} | ${'DearlyStars'} | ${''}           | ${''}        | ${[]}                                                       | ${['秋月涼', '日高愛', '水谷絵理']}
+    ${'アイドル名'} | ${''}            | ${'菊地真'}     | ${''}        | ${[]}                                                       | ${['菊地真']}
+    ${'ユニット名'} | ${''}            | ${'じゅぴたー'} | ${''}        | ${[]}                                                       | ${['Jupiter']}
+    ${'色味'}       | ${'DearlyStars'} | ${''}           | ${'#ff0000'} | ${[]}                                                       | ${['日高愛']}
+    ${'keep済み'}   | ${'keep'}        | ${''}           | ${'#800080'} | ${['mei_izumi_shinycolors', 'hinana_ichikawa_shinycolors']} | ${['和泉愛依']}
   `(
     '$testNameからの検索ができるか',
     ({ brand, name, similarColor, keeps, expected }) => {
