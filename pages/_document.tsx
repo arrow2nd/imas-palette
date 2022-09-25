8ef6df8c3f4359e9f599cbf43c23cca5ef4d481a
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-09-25 10:31:55 +0900

    feat: Cloudflare Web Analytics を追加 (#105)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,11 @@ class MyDocument extends Document {
             href="https://fonts.bunny.net/css?family=m-plus-rounded-1c:400"
             rel="stylesheet"
           />
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "7e995a4912664d1f96fba895f160587c"}'
+          />
         </Head>
         <body className="font-default text-natural-black bg-natural-white">
           <Main />
