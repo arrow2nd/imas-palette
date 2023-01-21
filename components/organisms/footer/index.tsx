d0b403a15aa87d204cdf523e0683ef1b9e6eec3a
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-01-21 22:57:39 +0900

    feat(components/footer): SNSのリンクを削除

--- components/organisms/footer/index.tsx
+++ components/organisms/footer/index.tsx
@@ -1,9 +1,22 @@
-import Links from './links'
+import { FiDatabase } from 'react-icons/fi'
+
+const Link = () => (
+  <a
+    className="flex flex-row items-center text-xl hover:text-imas transition-colors"
+    title="Database by im@sparql"
+    href="https://sparql.crssnky.xyz/imas"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <FiDatabase />
+    <span className="ml-2 text-sm">by im@sparql</span>
+  </a>
+)
 
 const Footer = () => (
   <div className="flex flex-col mt-20 items-center text-sm tracking-wide">
-    <Links />
-    <p className="mt-10 text-center">
+    <Link />
+    <p className="mt-6 text-center">
       The rights to all content related to THE IDOLM@STER belong to BANDAI NAMCO
       Entertainment Inc.
     </p>
