254cea4d3de3d88102a2872de918a396c8a5dd36
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-03-21 15:01:29 +0900

    feat: pnpm で Prettier のプラグインが使えない問題を修正

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,27 +1,27 @@
 // NOTE: pnpm で prettier のプラグインが解決できないため、暫定的に js で設定を書いてる
 // ref: https://zenn.dev/convcha/articles/6aa3dc18158a41
 module.exports = {
-  plugins: [require("trivago/prettier-plugin-sort-imports")],
+  plugins: [require('@trivago/prettier-plugin-sort-imports')],
   printWidth: 80,
   tabWidth: 2,
   useTabs: false,
   semi: false,
   singleQuote: true,
-  trailingComma: "none",
+  trailingComma: 'none',
   bracketSpacing: true,
-  arrowParens: "always",
+  arrowParens: 'always',
   importOrder: [
-    "<THIRD_PARTY_MODULES>",
-    "^components/atoms/(.*)$",
-    "^components/molecules/(.*)$",
-    "^components/organisms/(.*)$",
-    "^components/templates/(.*)$",
-    "^hooks/(.*)$",
-    "^data/(.*)$",
-    "^types/(.*)$",
-    "^styles/(.*)$",
-    "^[./]"
+    '<THIRD_PARTY_MODULES>',
+    '^components/atoms/(.*)$',
+    '^components/molecules/(.*)$',
+    '^components/organisms/(.*)$',
+    '^components/templates/(.*)$',
+    '^hooks/(.*)$',
+    '^data/(.*)$',
+    '^types/(.*)$',
+    '^styles/(.*)$',
+    '^[./]'
   ],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true
-};
+}
