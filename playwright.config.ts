9727eb78bf92aac4fd8c5690be215cecdc6265fc
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-01-14 15:34:31 +0900

    💚 リトライを追加

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { PlaywrightTestConfig, devices } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
   testDir: './e2e',
+  retry: 2,
   use: {
     baseURL: 'http://localhost:3000/'
   },
